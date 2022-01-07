3ef0904d66e642c10b3bd12e0b7475f68cdb6c39
Erika Lopes <erika.calu@tc.com.br>
2022-01-07 18:42:06 -0300

    add link api into useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,21 @@
 import { RepositoryItem } from "./RepositoryItem";
+import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
 
 export function RepositoryList() {
-  const repository = {
-    name: "unform",
-    description: "unform repository",
-    link: "https://github.com/unform/unform",
-  };
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos").then((response) =>
+      response.json().then((data) => setRepositories(data))
+    );
+  }, []);
+
   return (
-    <section className="respository-list">
+    <section className="repository-list">
       <h1>Lista de Repositórios</h1>
 
-      <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-      </ul>
+      <ul></ul>
     </section>
   );
 }
